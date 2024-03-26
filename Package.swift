b0a199ec4031ae7eb0f083816ce5d1ee65a91041
Andrei Ahibalau <andrei.ahibalau@intergiro.com>
2024-03-26 15:14:09 +0100

    Add Intergiro SDK (#1)
    * Add SPM and Cocoapods specs
    
    * Add license
    
    * Fix image

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "IntergiroSDK",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "IntergiroSDK",
+            targets: ["IntergiroSDK"]),
+    ],
+    targets: [
+        .binaryTarget(name: "IntergiroSDK", path: "Sources/IntergiroSDK.xcframework"),
+    ]
+)
---
+++ Sources/IntergiroSDK.xcframework/ios-arm64/IntergiroSDK.framework/Headers/IntergiroSDK.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for IntergiroSDK.
+FOUNDATION_EXPORT double IntergiroSDKVersionNumber;
+
+//! Project version string for IntergiroSDK.
+FOUNDATION_EXPORT const unsigned char IntergiroSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IntergiroSDK/PublicHeader.h>
+
+
